3dedba80968cb03754f1f9d72d0752962dd0f77e
dfsek <39248502+dfsek@users.noreply.github.com>
2019-09-29 22:20:38 -0700

    made actual form for 2019 game

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const createHTML = require("./lib/createHTML.js");
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const md5File = require('md5-file');
 const port = 80;
 const apiPort = 81;
 
@@ -15,45 +16,50 @@ var apiApp = new express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 
 var rawHtml = createHTML.generateHTML(jsonData);
 console.log(rawHtml);
-
 apiApp.get('/', function(req, res) {
-	res.send(rawHtml);
+  res.send(rawHtml);
 })
 
 apiApp.listen(apiPort);
 
-var html = "<form id='mainForm' action='/submit' method='POST'>" + createHTML.generateHTML(jsonData) + "<input type='submit' id='submit' value='Submit'/></form>";
-fs.writeFileSync("./html/form.html", html);
+
+fs.writeFileSync("./html/form.html", rawHtml);
+
+const hash = md5File.sync('./html/form.html');
+console.log("Hash of form is " + hash);
+
+var html = "<form id='mainForm' action='/submit' method='POST'>" + rawHtml + "<input type='submit' id='submit' value='Submit'/></form>";
+
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/html/form.html'));
+  res.send(html);
 });
 
 
 app.get('/analysis', function(req, res) {
-	res.sendFile(path.join(__dirname + '/html/form.html'));
+  res.sendFile(path.join(__dirname + '/html/form.html'));
 });
 
 
 app.post('/submit', function(req, res) {
 
-	console.log(req.body);
-	var dbArray = [];
-	try {
-		if(fs.existsSync("./db/db.json")) {
-			dbArray = JSON.parse(fs.readFileSync('./db/db.json'));
-		}
-	} catch {
-		fs.writeFileSync("./db/db.json", "");
-	}
-	dbArray.push(req.body);
-	fs.writeFileSync("./db/db.json", JSON.stringify(dbArray));
-	res.sendFile(path.join(__dirname + '/html/submit.html'));
+  console.log(req.body);
+  var dbArray = [];
+  try {
+    if (fs.existsSync("./db/db.json")) {
+      dbArray = JSON.parse(fs.readFileSync('./db/db.json'));
+    }
+  } catch {
+    fs.writeFileSync("./db/db.json", "");
+  }
+  dbArray.push(req.body);
+  fs.writeFileSync("./db/db.json", JSON.stringify(dbArray));
+  res.sendFile(path.join(__dirname + '/html/submit.html'));
 });
 
 console.log("listening on port " + port);

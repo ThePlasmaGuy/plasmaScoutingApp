705cdb10b9898ab6eb5e12da0f69186a6d4b8367
dfsek <39248502+dfsek@users.noreply.github.com>
2019-11-04 17:22:00 -0800

    added user separation

--- main.js
+++ main.js
@@ -10,9 +10,7 @@ const port = 80;
 const apiPort = 81;
 const debug = true;
 
-
-
-var jsonData = JSON.parse(fs.readFileSync('inputs.json', 'utf8'));
+var jsonData = JSON.parse(fs.readFileSync('./json/inputs.json', 'utf8'));
 var css = fs.readFileSync("./style.css");
 
 var rawHtml = createHTML.generateHTML([true, jsonData]);
@@ -21,26 +19,9 @@ console.log("HTML: " + createHTML.generateHTML([true, jsonData]));
 
 var scoutIDs = JSON.parse(fs.readFileSync('./json/scouts.json', 'utf8'));
 
-console.log(scoutIDs)
+console.log(scoutIDs);
 
-function isValidScoutID(id) {
-	for(var i = 0; i < scoutIDs.length; i++) {
-		if(scoutIDs[i].id == id) {
-			return true;
-			break;
-		}
-	}
-}
 
-function getScoutName(id) {
-	for(var i = 0; i < scoutIDs.length; i++) {
-		if(scoutIDs[i].id == id) {
-			return scoutIDs[i].name;
-			break;
-		}
-	}
-	return null;
-}
 
 var app = new express();
 
@@ -55,22 +36,12 @@ function randomValueHex(len) {
 		.slice(0, len)
 }
 
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
-
-
-
-if(debug === true) {
-	console.log(rawHtml);
-}
 apiApp.get('/', function(req, res) {
-	if(isValidScoutID(req.query.scoutID) === true) {
+	if(searchDB.isValidScoutID(req.query.scoutID) === true) {
 		res.send(rawHtml);
 	} else {
 		res.send('INVALID SCOUT ID');
@@ -78,7 +49,6 @@ apiApp.get('/', function(req, res) {
 })
 
 apiApp.listen(apiPort);
-
 
 fs.writeFileSync("./html/form.html", rawHtml);
 
@@ -94,31 +64,31 @@ try {
 }
 
 var sameHash0 = true;
-for(var i = 0; i < dbArray.length; i++) {
-	if(dbArray[i].hash === hash.toString()) {
-		sameHash0 = true;
-	} else {
-		sameHash0 = false;
-		break;
+var files = fs.readdirSync("./db")
+for(var i in files) {
+	try {
+		var testFile = JSON.parse(fs.readFileSync("./db/" + files[i]));
+		console.log(files[i]);
+		for(var j = 0; j < testFile.length; j++) {
+			console.log(testFile[j].hash);
+			if(testFile[j].hash !== hash.toString()) {
+				sameHash0 = false;
+			}
+		}
+	} catch {
+		fs.unlinkSync("./db/" + files[i]);
 	}
 }
-if(sameHash0 === true) {
-
-} else {
+if(sameHash0 === false) {
 	throw "ERROR: The database contains data from a form with a different hash than the form found!";
 }
 
-
-
 var html = "<!DOCTYPE html><html><head><style>" + css + "</style><title>Scouting App</title></head><body><form id='mainForm' action='/submit' method='POST'>" + rawHtml + "<input type='submit' id='submit' value='Submit'/></form><script type='text/javascript'>document.getElementById('mainForm').setAttribute('action', window.location.href)</script></body></html>";
 
-
-
-
 app.get('/', function(req, res) {
-	if(isValidScoutID(req.query.scoutID) === true) {
+	if(searchDB.isValidScoutID(req.query.scoutID) === true) {
 		res.send(html);
-		console.log("new client with IP " + req.ip + ", Scout ID " + req.query.scoutID + ", Name " + getScoutName(req.query.scoutID));
+		console.log("new client with IP " + req.ip + ", Scout ID " + req.query.scoutID + ", Name " + searchDB.getScoutName(req.query.scoutID) + ", page /");
 	} else if(!req.query.scoutID) {
 		res.sendFile(path.join(__dirname + '/html/login.html'));
 	} else {
@@ -127,9 +97,9 @@ app.get('/', function(req, res) {
 });
 
 app.get('/verify', function(req, res) {
-	if(isValidScoutID(req.query.scoutID) === true) {
+	if(searchDB.isValidScoutID(req.query.scoutID) === true) {
 		res.send('VALID');
-		console.log("new Electron client with IP " + req.ip + ", Scout ID " + req.query.scoutID + ", Name " + getScoutName(req.query.scoutID));
+		console.log("new Electron client with IP " + req.ip + ", Scout ID " + req.query.scoutID + ", Name " + searchDB.getScoutName(req.query.scoutID));
 	} else {
 		res.send('INVALID');
 	}
@@ -137,11 +107,12 @@ app.get('/verify', function(req, res) {
 
 
 app.get('/analysis', function(req, res) {
-	if(isValidScoutID(req.query.scoutID) === true) {
-		var rawHtmlNonRequire = createHTML.generateHTML([false, JSON.parse(fs.readFileSync("./inputs.json"))]);
+	if(searchDB.isValidScoutID(req.query.scoutID) === true) {
+		var rawHtmlNonRequire = createHTML.generateHTML([false, JSON.parse(fs.readFileSync("./json/inputs.json"))]);
 
 		var nonRequireHTML = "<!DOCTYPE html><html><head><style>" + css + "</style><title>Scouting App</title></head><body><form id='mainForm' action='/submitAnalysis' method='POST'>" + rawHtmlNonRequire + "<input type='submit' id='submit' value='Submit'/></form><script type='text/javascript'>document.getElementById('mainForm').setAttribute('action', window.location.href)</script></body></html>";
 		res.send(nonRequireHTML);
+		console.log("new client with IP " + req.ip + ", Scout ID " + req.query.scoutID + ", Name " + searchDB.getScoutName(req.query.scoutID) + ", page /analysis");
 		//
 	} else if(!req.query.scoutID) {
 		res.sendFile(path.join(__dirname + '/html/login.html'));
@@ -154,62 +125,10 @@ app.post('/analysis', function(req, res) {
 	res.send(searchDB.searchDB(JSON.parse(fs.readFileSync("./db/db.json")), req.body));
 });
 
-/*
-app.get('/analysis', function(req, res) {
-
-	if(isValidScoutID(req.query.scoutID) === true) {
-		var database = JSON.parse(fs.readFileSync('./db/db.json'));
-		if(req.query.team) {
-			if(req.query.match) {
-				console.log("TEAM: " + req.query.team + ", MATCH: " + req.query.match);
-				var responseString = "";
-				for(var i = 0; i < database.length; i++) {
-					if(database[i].data.teamNumber == req.query.team && database[i].data.matchNumber == req.query.match) {
-						responseString += "<pre class='prettyprint'>" + JSON.stringify(database[i].data, null, 4) + "</pre><br>";
-					}
-				}
-				if(responseString == "") {
-					responseString = "No data found matching request";
-				}
-				res.send(responseString);
-			} else {
-				console.log("TEAM: " + req.query.team);
-				var responseString = "";
-				for(var i = 0; i < database.length; i++) {
-					if(database[i].data.teamNumber == req.query.team) {
-						responseString += "<pre class='prettyprint'>" + JSON.stringify(database[i].data, null, 4) + "</pre><br>";
-					}
-				}
-				if(responseString == "") {
-					responseString = "No data found matching request";
-				}
-				res.send(responseString);
-			}
-		} else if(req.query.match) {
-			console.log("MATCH: " + req.query.match);
-			var responseString = "";
-			for(var i = 0; i < database.length; i++) {
-				if(database[i].data.matchNumber == req.query.match) {
-					responseString += "<pre class='prettyprint'>" + JSON.stringify(database[i].data, null, 4) + "</pre><br>";
-				}
-			}
-			if(responseString == "") {
-				responseString = "No data found matching request";
-			}
-			res.send(responseString);
-		} else {
-			res.sendFile(path.join(__dirname + '/html/query.html'));
-		}
-	} else if(!req.query.scoutID) {
-		res.sendFile(path.join(__dirname + '/html/login.html'));
-	} else {
-		res.sendFile(path.join(__dirname + '/html/loginIncorrect.html'));
-	}
-});
-*/
 
 app.get('/download', function(req, res) {
-	if(isValidScoutID(req.query.scoutID) === true) {
+	if(searchDB.isValidScoutID(req.query.scoutID) === true) {
+		console.log("new client with IP " + req.ip + ", Scout ID " + req.query.scoutID + ", Name " + searchDB.getScoutName(req.query.scoutID) + ", page /download");
 		var jsonMain;
 		try {
 			jsonMain = JSON.parse(fs.readFileSync('./db/db.json'));
@@ -243,7 +162,8 @@ app.get('/download', function(req, res) {
 });
 
 app.get('/downloadExcel', function(req, res) {
-	if(isValidScoutID(req.query.scoutID) === true) {
+	if(searchDB.isValidScoutID(req.query.scoutID) === true) {
+		console.log("new client with IP " + req.ip + ", Scout ID " + req.query.scoutID + ", Name " + searchDB.getScoutName(req.query.scoutID) + ", page /downloadExcel");
 		var jsonMain;
 		try {
 			jsonMain = JSON.parse(fs.readFileSync('./db/db.json'));
@@ -277,7 +197,7 @@ app.get('/downloadExcel', function(req, res) {
 });
 
 app.get('/submit', function(req, res) {
-	if(isValidScoutID(req.query.scoutID) === true) {
+	if(searchDB.isValidScoutID(req.query.scoutID) === true) {
 		if(debug === true) {
 			console.log(decodeURIComponent(req.query.data));
 		}
@@ -315,7 +235,7 @@ app.get('/submit', function(req, res) {
 				"uuid": getData.uuid,
 				"data": getData.data
 			}
-			dbPushObj.data.scoutID = req.query.scoutID;
+			dbPushObj.scoutID = req.query.scoutID;
 			dbArray.push(dbPushObj);
 			res.send('Form Submitted!');
 		} else if(uuidFound === true) {
@@ -332,10 +252,6 @@ app.get('/submit', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-
-	if(debug === true) {
-		console.log(req.body);
-	}
 	dbArray = [];
 	try {
 		if(fs.existsSync("./db/db.json")) {
@@ -360,8 +276,29 @@ app.post('/', function(req, res) {
 			"uuid": randomValueHex(256),
 			"data": req.body
 		}
-		dbNewObj.data.scoutID = req.query.scoutID;
-		dbArray.push(dbNewObj);
+		dbNewObj.scoutID = req.query.scoutID;
+		var separateUser = false
+		for(var i = 0; i < scoutIDs.length; i++) {
+			if(scoutIDs[i].id == req.query.scoutID) {
+				if(scoutIDs[i].separate === true) {
+					separateUser = true;
+				}
+				break;
+			}
+		}
+		if(separateUser === false) {
+			dbArray.push(dbNewObj);
+		} else {
+			console.log("User \"" + searchDB.getScoutName(req.query.scoutID) + "\" has the \"SEPARATE\" flag set to TRUE, submission will be saved to ./db/db" + req.query.scoutID + ".json instead of regular database.");
+			var separateJSON = []
+			try {
+				separateJSON = JSON.parse(fs.readFileSync("./db/db" + req.query.scoutID + ".json"));
+				separateJSON.push(dbNewObj);
+				fs.writeFileSync("./db/db" + req.query.scoutID + ".json", JSON.stringify(separateJSON));
+			} catch {
+				fs.writeFileSync("./db/db" + req.query.scoutID + ".json", JSON.stringify([dbNewObj]));
+			}
+		}
 		res.sendFile(path.join(__dirname + '/html/submit.html'));
 	} else {
 		console.log("Client attempted submitting file with incorrect hash. Does client have wrong file? has the form been accidentally updated?");

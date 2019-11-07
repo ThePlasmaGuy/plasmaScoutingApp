4d8df5d93635ee4035a368e86129ae948ff30316
dfsek <39248502+dfsek@users.noreply.github.com>
2019-11-06 17:06:57 -0700

    better logging

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const md5File = require('md5-file');
 const crypto = require('crypto');
+const readlineSync = require('readline-sync');
 const port = 80;
 const apiPort = 81;
 const debug = true;
@@ -13,14 +14,21 @@ const debug = true;
 var jsonData = JSON.parse(fs.readFileSync('./json/inputs.json', 'utf8'));
 var css = fs.readFileSync("./style.css");
 
-var rawHtml = createHTML.generateHTML([true, jsonData]);
+var rawHtml = createHTML.generateHTML(jsonData);
 
-console.log("HTML: " + createHTML.generateHTML([true, jsonData]));
+console.log("HTML: " + createHTML.generateHTML(jsonData) + "\n\n");
 
 var scoutIDs = JSON.parse(fs.readFileSync('./json/scouts.json', 'utf8'));
 
-console.log(scoutIDs);
-
+console.log("Scouts:");
+for(var i = 0; i < scoutIDs.length; i++) {
+	if(scoutIDs[i].separate === true) {
+		console.log("Name " + scoutIDs[i].name + ", ID " + scoutIDs[i].id + ", Separate=TRUE");
+	} else {
+		console.log("Name " + scoutIDs[i].name + ", ID " + scoutIDs[i].id + ", Separate=FALSE");
+	}
+}
+console.log("\n\n");
 
 
 var app = new express();
@@ -30,10 +38,7 @@ var apiApp = new express();
 var dbArray = [];
 
 function randomValueHex(len) {
-	return crypto
-		.randomBytes(Math.ceil(len / 2))
-		.toString('hex')
-		.slice(0, len)
+	return crypto.randomBytes(Math.ceil(len / 2)).toString('hex').slice(0, len);
 }
 
 app.use(bodyParser.json());
@@ -55,6 +60,11 @@ fs.writeFileSync("./html/form.html", rawHtml);
 const hash = md5File.sync('./html/form.html');
 console.log("Hash of form is " + hash);
 
+if(!fs.existsSync("./db/")) {
+	console.log("\n\n./db/ director does not exist, creating directory.\n\n");
+	fs.mkdirSync("./db/");
+}
+
 try {
 	if(fs.existsSync("./db/db.json")) {
 		dbArray = JSON.parse(fs.readFileSync('./db/db.json'));
@@ -68,19 +78,41 @@ var files = fs.readdirSync("./db")
 for(var i in files) {
 	try {
 		var testFile = JSON.parse(fs.readFileSync("./db/" + files[i]));
-		console.log(files[i]);
+		console.log("Checking hashes of file " + files[i]);
 		for(var j = 0; j < testFile.length; j++) {
-			console.log(testFile[j].hash);
+			console.log("Hash of entry " + j + " of " + files[i] + " is " + testFile[j].hash);
 			if(testFile[j].hash !== hash.toString()) {
 				sameHash0 = false;
 			}
 		}
+		console.log("\n");
 	} catch {
 		fs.unlinkSync("./db/" + files[i]);
 	}
 }
+console.log("\n");
 if(sameHash0 === false) {
-	throw "ERROR: The database contains data from a form with a different hash than the form found!";
+	var delDB = readlineSync.question("Database contains mismatching hashes. Delete files with incorrect hashes? (Y/N): ");
+	if(delDB === "Y") {
+		for(var i in files) {
+			try {
+				var testFile = JSON.parse(fs.readFileSync("./db/" + files[i]));
+				console.log("Checking hashes of file " + files[i]);
+				for(var j = 0; j < testFile.length; j++) {
+					console.log("Hash of entry " + j + " of " + files[i] + " is " + testFile[j].hash);
+					if(testFile[j].hash !== hash.toString()) {
+						fs.unlinkSync("./db/" + files[i]);
+						break;
+					}
+				}
+				console.log("\n");
+			} catch {
+				fs.unlinkSync("./db/" + files[i]);
+			}
+		}
+	} else {
+		throw "ERROR: The database contains data from a form with a different hash than the form found!";
+	}
 }
 
 var html = "<!DOCTYPE html><html><head><style>" + css + "</style><title>Scouting App</title></head><body><form id='mainForm' action='/submit' method='POST'>" + rawHtml + "<input type='submit' id='submit' value='Submit'/></form><script type='text/javascript'>document.getElementById('mainForm').setAttribute('action', window.location.href)</script></body></html>";
@@ -108,7 +140,7 @@ app.get('/verify', function(req, res) {
 
 app.get('/analysis', function(req, res) {
 	if(searchDB.isValidScoutID(req.query.scoutID) === true) {
-		var rawHtmlNonRequire = createHTML.generateHTML([false, JSON.parse(fs.readFileSync("./json/inputs.json"))]);
+		var rawHtmlNonRequire = createHTML.generateAnalysisHTML(JSON.parse(fs.readFileSync("./json/inputs.json")));
 
 		var nonRequireHTML = "<!DOCTYPE html><html><head><style>" + css + "</style><title>Scouting App</title></head><body><form id='mainForm' action='/submitAnalysis' method='POST'>" + rawHtmlNonRequire + "<input type='submit' id='submit' value='Submit'/></form><script type='text/javascript'>document.getElementById('mainForm').setAttribute('action', window.location.href)</script></body></html>";
 		res.send(nonRequireHTML);
@@ -122,7 +154,11 @@ app.get('/analysis', function(req, res) {
 });
 
 app.post('/analysis', function(req, res) {
-	res.send(searchDB.searchDB(JSON.parse(fs.readFileSync("./db/db.json")), req.body));
+	try {
+		res.send(searchDB.searchDB(JSON.parse(fs.readFileSync("./db/db.json")), req.body));
+	} catch {
+		res.send("ERROR");
+	}
 });
 
 
@@ -277,7 +313,7 @@ app.post('/', function(req, res) {
 			"data": req.body
 		}
 		dbNewObj.scoutID = req.query.scoutID;
-		var separateUser = false
+		var separateUser = false;
 		for(var i = 0; i < scoutIDs.length; i++) {
 			if(scoutIDs[i].id == req.query.scoutID) {
 				if(scoutIDs[i].separate === true) {
@@ -290,7 +326,7 @@ app.post('/', function(req, res) {
 			dbArray.push(dbNewObj);
 		} else {
 			console.log("User \"" + searchDB.getScoutName(req.query.scoutID) + "\" has the \"SEPARATE\" flag set to TRUE, submission will be saved to ./db/db" + req.query.scoutID + ".json instead of regular database.");
-			var separateJSON = []
+			var separateJSON = [];
 			try {
 				separateJSON = JSON.parse(fs.readFileSync("./db/db" + req.query.scoutID + ".json"));
 				separateJSON.push(dbNewObj);
@@ -309,5 +345,5 @@ app.post('/', function(req, res) {
 
 });
 
-console.log("listening on port " + port);
+console.log("listening on port " + port + "\n\n\n\n\n");
 app.listen(port);
